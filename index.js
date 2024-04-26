d00ad07ce6f89bd22a0cfd5802a39d8e1da3d524
Rasel9360 <raselmahamud9360@gmail.com>
2024-04-26 22:12:56 +0600

    mongodb indivisul id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,6 +36,13 @@ async function run() {
     app.get('/spots', async(req, res)=>{
         const curser = spotsCollection.find();
         const result = await  curser.toArray();
+        res.send(result)
+    })
+
+    app.get('/spots/:id', async(req, res)=>{
+        const id =  req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await  spotsCollection.findOne(query);
         res.send(result)
     })
 
