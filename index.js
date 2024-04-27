dbc38566354a836af923a42f97c9594ca9b9d148
Rasel9360 <raselmahamud9360@gmail.com>
2024-04-27 23:11:35 +0600

    delete functionality added

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ async function run() {
       res.send(result);
     })
 
+    // delete data
+    app.delete('/myProduct/:id', async (req,res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      console.log(query);
+      const result = await spotsCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
